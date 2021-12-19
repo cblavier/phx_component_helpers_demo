d2070cf87295d56b10cc7b36300861eafe1df79a
Christian Blavier <cblavier@gmail.com>
2021-12-19 18:48:06 +0100

    page title

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './js/**/*.js',
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
